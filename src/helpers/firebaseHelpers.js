99e39b47c2aff235c1ca5380eebe9d3462756f6c
hobywhan <hobywhan@free.fr>
2016-12-20 23:34:10 +0100

    Start of the project, use of boilerplate on main technology - vue + firebase

---
+++ src/helpers/firebaseHelpers.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase'
+
+export function fireInit(func) {
+	// !!!! Replace with your own Firebase settings !!!!!
+	var config = {
+		apiKey: 'AIzaSyDwJ1XZk23i79ePjEd5jXqBw_fObVGscUQ',
+    authDomain: 'peoplefundingistef.firebaseapp.com',
+    databaseURL: 'https://peoplefundingistef.firebaseio.com',
+    storageBucket: 'peoplefundingistef.appspot.com',
+    messagingSenderId: '353174719687'
+	}
+	firebase.initializeApp(config)
+
+	firebase.auth().onAuthStateChanged(function (user) {
+	  if (user) {
+	    func(true, user)
+	  } else {
+	    func(false)
+	  }
+	}, function(error) {
+	  console.log(error)
+	})
+}
